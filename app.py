3911b5c21be540fbe9c3c91ad5c23fe7c5b34b7a
Antonio-Tone <Antoniotone815@gmail.com>
2024-07-30 21:08:14 +0200

    api

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ load_dotenv()
 app = Flask(__name__)
 
 # Import the blueprint
+# Import the blueprint
 from routes.users import users_bp
 from routes.movies import movies_bp
 from routes.orders import orders_bp
 
-# Register the blueprint
-app.register_blueprint(users_bp, url_prefix='/api')
-app.register_blueprint(movies_bp, url_prefix='/api')
-app.register_blueprint(orders_bp, url_prefix='/api')
+# Register the blueprint without a URL prefix
+app.register_blueprint(users_bp)
+app.register_blueprint(movies_bp)
+app.register_blueprint(orders_bp)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
